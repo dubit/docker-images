021892540efb4238852609e68727fd6f69f58174
Stefan Bobev <stefanbobev@hotmail.com>
2018-03-15 09:27:15 +0000

    Modified the build script to include the new additions

--- android-build/build.sh
+++ android-build/build.sh
@@ -3,18 +3,26 @@
 CURRENT_DIR=$(pwd)
 DIRECTORY_SEPARATOR="/"
 
-base_images=(sdk-23 sdk-25)
-ndk_images=(sdk-23-ndk sdk-25-ndk)
+base_images=(base)
+sdk_only_images=(sdk-23 sdk-25 sdk-27)
+ndk_images=(sdk-23-ndk sdk-25-ndk sdk-27-ndk)
 
-# Build the base images first.
+# Build the base image first.
 for i in "${base_images[@]}"
 do
   cd "$CURRENT_DIR""$DIRECTORY_SEPARATOR""$i"
   docker build -t dubit/android-build-image:"$i" .
 done
 
-# Build the NDK based images second since those are
-# based on the base ones.
+# Build the sdk only images next.
+for i in "${sdk_only_images[@]}"
+do
+  cd "$CURRENT_DIR""$DIRECTORY_SEPARATOR""$i"
+  docker build -t dubit/android-build-image:"$i" .
+done
+
+# Build the NDK based images last since those are
+# based on the sdk ones.
 for n in "${ndk_images[@]}"
 do
   cd "$CURRENT_DIR""$DIRECTORY_SEPARATOR""$n"
